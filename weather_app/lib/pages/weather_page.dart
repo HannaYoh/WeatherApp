fb495d747839af4447acefd99ce6df3e9c76d664
Hanna Yohannes <hannayohmek@gmail.com>
2025-02-10 22:33:43 +0300

    weather_page updated

--- weather_app/lib/pages/weather_page.dart
+++ weather_app/lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/services/weather_service.dart';
+import 'package:weather_app/models/weather_model.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,6 +10,28 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService(apiKey);
+  Weather? _weather;
+
+  //fetch weather
+  _fetchWeather() async {
+    //get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    //errors
+    catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
